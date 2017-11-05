c59fc7f3effdfc2663a9ead43c9936f905de805e
Benyapa Emsawat <benyapae57@email.nu.ac.th>
2017-11-05 15:52:14 +0700

    setting incomplete

--- RegDemo/SettingViewController.swift
+++ RegDemo/SettingViewController.swift
@@ -8,8 +8,33 @@
 
 import UIKit
 import FirebaseAuth
+import AlamofireImage
+import Alamofire
 
 class SettingViewController: UIViewController {
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var studentIDLabel: UILabel!
+    @IBOutlet weak var notificationSwitch: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        showInfomation()
+    }
+    
+    
+    @IBAction func editNickname() {
+        
+    }
+    
+    func showInfomation () {
+        if let token = AuthenticationManager.token(), let request = API.profileImageURLRequest(token: token) {
+            profileImage.af_setImage(withURLRequest: request)
+        }
+        studentIDLabel.text = AuthenticationManager.user()?.uid
+        nameLabel.text = AuthenticationManager.user()?.name
+    }
     
     @IBAction func logout() {
         do {
