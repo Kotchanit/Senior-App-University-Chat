04d5942ab4bc5eb02d21123b2b5d44191994f63e
Benyapa Emsawat <benyapae57@email.nu.ac.th>
2017-11-21 19:45:33 +0700

    set color tabbar

---
+++ RegDemo/TabbarViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TabbarViewController.swift
+//  RegDemo
+//
+//  Created by B13 on 11/21/2560 BE.
+//  Copyright © 2560 Apptitude. All rights reserved.
+//
+
+import UIKit
+
+class TabbarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.tabBar.unselectedItemTintColor = UIColor.white
+    }
+
+}
